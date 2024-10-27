7d52626fec68951db83aa606f5205111ea567a75
Shneyd22 <126271500+Shneyd22@users.noreply.github.com>
2024-10-27 15:55:37 +0300

    hw1 is done

---
+++ React_homeworks/hw1/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Message from './Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message name="Sergei" />
+      <Message name="Alex" />
+      <Message name="guys" />
+    </div>
+  );
+}
+
+export default App;
---
+++ React_homeworks/hw1/Message.js
@@ -0,0 +1,7 @@
+function Message({ name }) {
+    return (
+        <h1 className="messageElem">Hello {name}!</h1>
+    );
+}
+
+export default Message;
